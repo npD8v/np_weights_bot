b45f875d9d5c1c8540ecde35938f8d3415dbad86
npD8v <npdev77@gmail.com>
2023-12-14 16:12:42 +0200

    synch

--- start.bat
+++ start.bat
@@ -1,9 +1,22 @@
+echo Attempting to kill node.exe && ngrok.exe porccessses...
+taskkill /F /IM node.exe
+taskkill /F /IM ngrok.exe
 
 @echo off
-start /B ngrok http 8000
+timeout /t 1 /nobreak
+echo Going to C:\Users\y.tomchuk\Documents\CODEBASE\np_weights_bot...
+cd /d "C:\Users\y.tomchuk\Documents\CODEBASE\np_weights_bot"
 
-timeout /t 10 /nobreak
+timeout /t 1 nobreak
+echo Attempting to start ngrok.exe on port 8000...
+start ngrok http 8000
 
-start node src/app.js
+timeout /t 1 /nobreak
+echo Attempting to start node server...
+start cmd /k "node src\app.js"
 
-pause
+taskkill /F /IM cmd.exe
+
+timeout /t 1 /nobreak
+echo OK
+timeout /t 1 /nobreak
